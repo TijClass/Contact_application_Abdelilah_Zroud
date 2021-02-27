3fcd3052c8f7af0cefb4f34d4d8fc84e8f26a63d
ZroudAbdelilah <76938527+ZroudAbdelilah@users.noreply.github.com>
2021-02-27 16:43:09 +0100

    préparation php pour add-person

---
+++ add_person.php
@@ -0,0 +1,8 @@
+<?php
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$adresse = $_POST['adresse'];
+$phone = $_POST['phone'];
+$radio = $_POST['inlineRadioOptions'];
+$notes = $_POST['notes'];
