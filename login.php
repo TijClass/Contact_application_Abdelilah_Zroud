e401de59586d03e852c44a685bf494ed4d8ed0d6
ZroudAbdelilah <76938527+ZroudAbdelilah@users.noreply.github.com>
2021-03-02 09:35:13 +0100

    verifier log in par Base donneé

--- login.php
+++ login.php
@@ -1,21 +1,35 @@
 <?php
+require('./connection.php');
 if(isset($_POST)){
     // get from data base
     $sql = "SELECT * FROM users";
-    
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        $row = mysqli_fetch_assoc($result);
+          $logEmail = $row["email"];
+          $logPassword = $row["password"];
+      }else{
+          echo "error";
+      }
+      
 
     //
     $email = $_POST['email'];
     $password = $_POST['password'];
     if($email != "" && $password != "" ){
-        if ($email == "aby@anywhere.com"){
-                if ($password == "1234"){
+        if ($email == $logEmail){
+                if ($password == $logPassword){
                     session_start(); 
                     $_SESSION['login']=true;
-                    setcookie("login_email",$email, time() + (86400 * 30), "/");
-                    setcookie("login_password",$password, time() + (86400 * 30), "/");
+                    if(isset($_POST['remember'])){
+                        setcookie("login_email",$email, time() + (86400 * 30), "/");
+                        setcookie("login_password",$password, time() + (86400 * 30), "/");
+                    }
                     header('Location: ./index.php');
                 }
+            }else{
+                echo "wrong email";
             }
     }
 }else{
